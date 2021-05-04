0ea1c9411de6e2057f717ee100e1fcf0ae997011
Mrinal Haque <mrinalhaque99@gmail.com>
2021-05-04 16:57:40 +0600

    Update admin-menu-fix.js
    menuFix function updated to remove jquery dependency and https://standardjs.com/ maintain

--- src/admin/utils/admin-menu-fix.js
+++ src/admin/utils/admin-menu-fix.js
@@ -1,34 +1,26 @@
 /**
  * As we are using hash based navigation, hack fix
  * to highlight the current selected menu
- *
- * Requires jQuery
  */
-function menuFix(slug) {
-    var $ = jQuery;
+ function menuFix (slug) {
+  const menuRoot = document.querySelector(`#toplevel_page_${slug}`)
+  const currentUrl = window.location.href
+  const currentPath = currentUrl.substr(currentUrl.indexOf('admin.php'))
 
-    let menuRoot = $('#toplevel_page_' + slug);
-    let currentUrl = window.location.href;
-    let currentPath = currentUrl.substr( currentUrl.indexOf('admin.php') );
-
-    menuRoot.on('click', 'a', function() {
-        var self = $(this);
-
-        $('ul.wp-submenu li', menuRoot).removeClass('current');
-
-        if ( self.hasClass('wp-has-submenu') ) {
-            $('li.wp-first-item', menuRoot).addClass('current');
+  menuRoot.addEventListener('click', function (e) {
+    const target = e.target
+    this.querySelectorAll('li').forEach(
+      function (node) {
+        if (node !== e.target.parentElement) {
+          node.classList.remove('current')
         } else {
-            self.parents('li').addClass('current');
+          target.parentElement.classList.add('current')
         }
-    });
+      }
+    )
+  })
 
-    $('ul.wp-submenu a', menuRoot).each(function(index, el) {
-        if ( $(el).attr( 'href' ) === currentPath ) {
-            $(el).parent().addClass('current');
-            return;
-        }
-    });
+  menuRoot.querySelector(`.wp-submenu a[href="${currentPath}"`).parentElement.classList.add('current')
 }
 
-export default menuFix;
+export default menuFix
